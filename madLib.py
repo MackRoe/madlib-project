faa1b007b3aac685528796a8cae4b9fcc6d7d7fd
Elaine Music <Elaine.music@students.makeschool.com>
2019-08-28 20:20:48 -0700

    "add additional word type inputs"

--- madLib.py
+++ madLib.py
@@ -1,1 +1,5 @@
 noun = input("Enter a noun: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb (word that modifies an adjective, usually ending in -ly):")
+proper-noun-place = input("Enter the name of a Place:")
