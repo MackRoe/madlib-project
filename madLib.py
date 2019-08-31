2f855ec3dba9d60dca6dc9715910b99d11368b5a
MackRoe <elaine.music@students.makeschool.com>
2019-08-30 18:51:56 -0700

    second attempt to recover previous work

--- madLib.py
+++ madLib.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-noun = input("Enter a noun:")
-print noun
-=======
+
 noun = input("Enter a noun: ")
 adjective = input("Enter an adjective: ")
 verb = input("Enter a verb: ")
 adverb = input("Enter an adverb (word that modifies an adjective, usually ending in -ly):")
 proper-noun-place = input("Enter the name of a Place:")
->>>>>>> faa1b007b3aac685528796a8cae4b9fcc6d7d7fd
