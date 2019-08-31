89bcc3245c9a6a1dc0995f416d0aa8add4e5c49e
MackRoe <elaine.music@students.makeschool.com>
2019-08-30 19:08:40 -0700

    update to comment out non-functional code

--- madLib.py
+++ madLib.py
@@ -3,4 +3,14 @@ noun = input("Enter a noun: ")
 adjective = input("Enter an adjective: ")
 verb = input("Enter a verb: ")
 adverb = input("Enter an adverb (word that modifies an adjective, usually ending in -ly):")
-proper-noun-place = input("Enter the name of a Place:")
+proper_noun_place = input("Enter the name of a Place:")
+first_number = input("Enter a number: ")
+second_number = input("Enter another number: ")
+third_number = input("Please enter one more number: ")
+
+#define madlibs to be filled
+#madlibs = []
+#madlibs.append("Test {}, {}, {}")
+#test so far
+#print(madlibs(0)).format(first_number, second_number, third_number)
+#none of that worked right
